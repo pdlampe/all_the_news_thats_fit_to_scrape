ae7118a7dfca3513a12aded042569027ec607fd1
Paul David Lampe <pdlampe@gmail.com>
2019-10-06 14:17:48 -0500

    A GET route for scraping the NYTimes website

--- server.js
+++ server.js
@@ -7,6 +7,51 @@ var exphbs = require("express-handlebars");
 
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost.mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
+app.get("/scrape", function (req, res) {
+
+    axios.get("https://www.nytimes.com/").then(function (response) {
+
+        var $ = cheerio.load(response.data);
+
+
+        $("article h2").each(function (i, element) {
+
+            var result = {};
+
+            result.title = $(this)
+                .children("a")
+                .text();
+            result.link = $(this)
+                .children("a")
+                .attr("href");
+
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+
+                    console.log(err);
+                });
+        });
+        res.send("Scrape Complete!");
+    });
+});
+
+// Start the server
+app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!");
+});
