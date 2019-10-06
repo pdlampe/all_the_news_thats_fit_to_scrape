b9b4c1e26d0f55b83962b355b05cf3e84bdc8db1
Paul David Lampe <pdlampe@gmail.com>
2019-10-06 13:23:38 -0500

    Created server JS file

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+var exphbs = require("express-handlebars");
+
+var db = require("./models");
+
+var PORT = 3000;
+
+var app = express();
