451cd8ce7b10fd6e48e4f5a3cc33e6d9b8fccc52
Paul David Lampe <pdlampe@gmail.com>
2019-10-10 18:18:09 -0500

    Updated server JS file

--- server.js
+++ server.js
@@ -13,9 +13,13 @@ var app = express();
 
 app.use(logger("dev"));
 
+app.use(express.static("public"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static("public"));
+
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost.mongoHeadlines";
 
 mongoose.connect(MONGODB_URI);
@@ -37,9 +41,16 @@ app.get("/scrape", function (req, res) {
             result.link = $(this)
                 .children("a")
                 .attr("href");
+            result.story = $(this)
+                .parent()
+                .children("div .smlpost-story")
+                .children("p")
+                .text();
+
 
             db.Article.create(result)
                 .then(function (dbArticle) {
+
                     console.log(dbArticle);
                 })
                 .catch(function (err) {
@@ -47,9 +58,21 @@ app.get("/scrape", function (req, res) {
                     console.log(err);
                 });
         });
-        res.send("Scrape Finished!");
+
+        res.redirect("back");
     });
 });
+
+app.get("/articles", function (req, res) {
+    db.Article.find({})
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
+});
+
 
 app.get("/articles/:id", function (req, res) {
 
@@ -63,11 +86,12 @@ app.get("/articles/:id", function (req, res) {
         });
 });
 
-app.post("/articles/:id", function (req, res) {
 
+app.post("/articles/:id", function (req, res) {
     db.Note.create(req.body)
         .then(function (dbNote) {
-            return db.Article.findOneAndUpdate({ _id: req.params.id }, { $push: { note: dbPopulate._id } }, { new: true });
+
+            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
         })
         .then(function (dbArticle) {
             res.json(dbArticle);
@@ -78,7 +102,13 @@ app.post("/articles/:id", function (req, res) {
         });
 });
 
-// Start the server
+app.get("/clear", function (req, res) {
+    db.Article.deleteMany({}, function (err) {
+        if (err) return handleError(err);
+    });
+    res.redirect("back");
+})
+
 app.listen(PORT, function () {
     console.log("App running on port " + PORT + "!");
 });
