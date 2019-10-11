40932a905ff4bab19025afd4327910352c570573
Paul David Lampe <pdlampe@gmail.com>
2019-10-10 23:29:01 -0500

    Updated server JS file

--- server.js
+++ server.js
@@ -20,94 +20,19 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+
+
+
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost.mongoHeadlines";
 
 mongoose.connect(MONGODB_URI);
 
-app.get("/scrape", function (req, res) {
+var routes = require("./controllers/controller.js");
 
-    axios.get("https://www.nytimes.com/").then(function (response) {
-
-        var $ = cheerio.load(response.data);
+app.use('/', routes);
 
 
-        $("article h2").each(function (i, element) {
-
-            var result = {};
-
-            result.title = $(this)
-                .children("a")
-                .text();
-            result.link = $(this)
-                .children("a")
-                .attr("href");
-            result.story = $(this)
-                .parent()
-                .children("div .smlpost-story")
-                .children("p")
-                .text();
-
-
-            db.Article.create(result)
-                .then(function (dbArticle) {
-
-                    console.log(dbArticle);
-                })
-                .catch(function (err) {
-
-                    console.log(err);
-                });
-        });
-
-        res.redirect("back");
-    });
-});
-
-app.get("/articles", function (req, res) {
-    db.Article.find({})
-        .then(function (dbArticle) {
-            res.json(dbArticle);
-        })
-        .catch(function (err) {
-            res.json(err);
-        });
-});
-
-
-app.get("/articles/:id", function (req, res) {
-
-    db.Article.findOne({ _id: req.params.id })
-        .populate("note")
-        .then(function (dbArticle) {
-            res.json(dbArticle);
-        })
-        .catch(function (err) {
-            res.json(err);
-        });
-});
-
-
-app.post("/articles/:id", function (req, res) {
-    db.Note.create(req.body)
-        .then(function (dbNote) {
-
-            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-        })
-        .then(function (dbArticle) {
-            res.json(dbArticle);
-        })
-        .catch(function (err) {
-
-            res.json(err);
-        });
-});
-
-app.get("/clear", function (req, res) {
-    db.Article.deleteMany({}, function (err) {
-        if (err) return handleError(err);
-    });
-    res.redirect("back");
-})
 
 app.listen(PORT, function () {
     console.log("App running on port " + PORT + "!");
