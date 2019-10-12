8c2bc42a070c65cee486fbe4393b3a82d14f8cc9
Paul David Lampe <pdlampe@gmail.com>
2019-10-12 17:40:27 -0500

    Updated app JS file

--- public/app.js
+++ public/app.js
@@ -1,58 +1,40 @@
-$.getJSON("/articles", function (data) {
-
-    for (var i = 0; i < data.length; i++) {
-
-        $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+$.get("/api/articles", function (data, err) {
+    if (err) {
+        console.log(err);
     }
-});
-
-
-
-$(document).on("click", "p", function () {
-    $("#notes").empty();
-    var thisId = $(this).attr("data-id");
-
-    $.ajax({
-        method: "GET",
-        url: "/articles/" + thisId
-    })
-        .then(function (data) {
-            console.log(data);
-            $("#notes").append("<h2>" + data.title + "</h2>");
-            $("#notes").append("<input id='titleinput' name='title' >");
-            $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-            $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-
-            if (data.note) {
-                $("#titleinput").val(data.note.title);
-                $("#bodyinput").val(data.note.body);
-            }
-        });
-});
-
-
-
-$(document).on("click", "#savenote", function () {
-    var thisId = $(this).attr("data-id");
-
-    $.ajax({
-        method: "POST",
-        url: "/articles/" + thisId,
-        data: {
-
-            title: $("#titleinput").val(),
-
-            body: $("#bodyinput").val()
+    console.log(data);
+    data.forEach(article => {
+        let newsStory = $("#template").clone().removeAttr("id").removeAttr("hidden");
+        newsStory.children("article").attr("data-id", article._id);
+        newsStory.children("article").attr("data-fav", article.favorite)
+            .find("i")
+            .removeClass(article.favorite ? "far" : "fas")
+            .addClass(article.favorite ? "fas" : "far");
+        newsStory.find(".article-headline").text(article.headline);
+        newsStory.find(".article-category").text(article.category);
+        newsStory.find(".article-author").text(article.author);
+        newsStory.find(".card-body").children(".btn-primary").attr("href", "https://www.nytimes.com/" + article.url);
+        if (article.image) {
+            newsStory.find("img").attr("src", article.image);
         }
-    })
+        else {
+            newsStory.find("img").attr("hidden", true);
+        }
 
-        .then(function (data) {
+        $("#articles").children(".row").append(newsStory);
+    });
+});
 
-            console.log(data);
-
-            $("#notes").empty();
-        });
-
-    $("#titleinput").val("");
-    $("#bodyinput").val("");
+$(document).on("click", "i", function () {
+    let id = $(this).closet("article").attr("data-id");
+    let isFav = Boolean($(this).closet("article").attr("data-fav") === 'true');
+    console.log($(this).closest("article").attr("data-fav"));
+    console.log("id" + id + " faved: " + isFav);
+    $.ajax({
+        type: 'PUT',
+        url: '/api/favorite/' + id,
+        data: { favorite: !isFav }
+    }).done(data => {
+        $(this).toggleClass("far").toggleClass("fas")
+    });
 });
